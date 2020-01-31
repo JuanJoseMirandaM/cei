c5374f1491ef716a3bfb388662abc9926761418a
JuanJoseMirandaM <jjsmm97@gmail.com>
2020-01-31 15:47:58 -0400

    Updates

---
+++ precache-manifest.3e9452289a7d7b2197c6745b5fbe1e03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc8cc2d1814d50b08dff1d6cd0ba023d",
+    "url": "/cei/index.html"
+  },
+  {
+    "revision": "7492c52991af2d89a866",
+    "url": "/cei/static/css/2.3a4755b7.chunk.css"
+  },
+  {
+    "revision": "a4a3df889fe4a1cf0f37",
+    "url": "/cei/static/css/main.fe9ca9e7.chunk.css"
+  },
+  {
+    "revision": "7492c52991af2d89a866",
+    "url": "/cei/static/js/2.557abced.chunk.js"
+  },
+  {
+    "revision": "a4a3df889fe4a1cf0f37",
+    "url": "/cei/static/js/main.3fb7b550.chunk.js"
+  },
+  {
+    "revision": "5e19a42b66de016f165d",
+    "url": "/cei/static/js/runtime~main.81c3da57.js"
+  }
+]);
